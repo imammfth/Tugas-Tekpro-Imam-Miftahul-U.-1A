6640d3ad462e4db4b160d45b78cf6bbb033d98ff
imammfth <imammiftahululum200@gmail.com>
2026-04-12 19:38:40 +0700

    Add files via upload

---
+++ week 7 nmr 1/TransferFulus.java
@@ -0,0 +1,47 @@
+class Account {
+    int balance = 150;
+}
+
+public class TransferFulus {
+    public static void main(String[] args) throws InterruptedException {
+        Account acc1 = new Account();
+        Account acc2 = new Account();
+
+        // Thread 1: Menjumlahkan/ transfer fulus dari acc1 ke acc2
+        Thread t1 = new Thread(() -> {
+            synchronized (acc1) { // Mengunci acc1
+                System.out.println("Thread 1: Mengunci acc1, bersiap transfer...");
+                try { Thread.sleep(100); } catch (Exception e) {} 
+
+                synchronized (acc2) { // Mengunci acc2
+                    System.out.println("Thread 1: Mengunci acc2, memproses transfer dari acc1 ke acc2.");
+                    acc2.balance += acc1.balance;
+                }
+            }
+        });
+
+        // Thread 2: Menjumlahkan/ transfer fulus dari acc2 ke acc1
+        Thread t2 = new Thread(() -> {
+            // SOLUSI: Urutan penguncian disamakan dengan Thread 1 untuk mencegah deadlock
+            synchronized (acc1) { // Mengunci acc1 terlebih dahulu
+                System.out.println("Thread 2: Mengunci acc1, bersiap transfer...");
+                try { Thread.sleep(100); } catch (Exception e) {}
+
+                synchronized (acc2) { // Mengunci acc2 kemudian
+                    System.out.println("Thread 2: Mengunci acc2, memproses transfer dari acc2 ke acc1.");
+                    acc1.balance += acc2.balance;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("--- HASIL AKHIR ---");
+        System.out.println("Saldo Akhir acc1: " + acc1.balance);
+        System.out.println("Saldo Akhir acc2: " + acc2.balance);
+    }
+}
